45d80538d51b553f2cbe8eadafaf9710a6d51948
thorfernbacher <thorfernbacher@gmail.com>
2019-01-18 14:44:36 -0500

    auto update

--- release.sh
+++ release.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # current Git branch
-branch=$(git describe --tag)
+branch=$(git describe --tags --abbrev=0)
 url=$(git config --get remote.origin.url)
 echo $branch
 git checkout gh-pages
 if [ ! -d "$branch" ]; then
-  # Control will enter here if $DIRECTORY exists.
-  git submodule add -b master $url $branch
+ 	 # Control will enter here if $DIRECTORY exists.
+  	git submodule add -b master $url $branch
 else
 	git submodule update $branch
 fi
