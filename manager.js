dce9710e6fe01d226769b2b2d15e2f991458f162
thorfernbacher <thorfernbacher@gmail.com>
2019-01-21 20:02:35 -0500

    update

--- manager.js
+++ manager.js
@@ -13,6 +13,10 @@ const wd = process.cwd(),
 	name = wd.slice(wd.lastIndexOf('\\') + 1);
 if (wd == __dirname)
 	return;
+if (!fs.existsSync(`${wd}/.git`)) {
+	console.log('The current folder does not contain a git repository');
+	process.exit();
+}
 let args = process.argv,
 	operations = {
 		update,
@@ -35,13 +39,9 @@ if (args[2]) {
 function add() {
 	//check if the project already exits
 	if (!fs.existsSync(`${__dirname}/projects/${name}`)) {
-		run(`git submodule add -f -b master https://github.com/trobol/${name} projects/${name}/latest`, __dirname)
-			.catch(e => console.log('Error', e))
-			.then(() => {
-				run(['git', 'commit', `--message=Added ${name} to site`], __dirname)
-					.then(() => run(`git push`, __dirname).catch(e => console.log(e)))
-					.catch(e => console.log(e));
-			})
+		run(`git submodule add -f -b master https://github.com/trobol/${name} projects/${name}/latest`, __dirname);
+		run(['git', 'commit', `--message=Added ${name} to site`], __dirname);
+		run(`git push`, __dirname);
 	} else {
 		console.log(`Project: ${name} already exits`)
 	}
@@ -55,91 +55,69 @@ function update() {
 	}
 	let version = 'latest';
 	console.log(`Working in ${name}`);
-	run('git describe --tags --abbrev=0', `${__dirname}/projects/${name}/latest`)
-		.catch((error) => {
-			console.log('No Tags');
-		})
-		.then((v) => {
+	let v = run('git describe --tags --abbrev=0', `${__dirname}/projects/${name}/latest`).stdout.toString();
 
-			if (v)
-				version = v.slice(0, v.length - 1);
-			if (!fs.existsSync(`${wd}/.git`)) {
-				console.log('The current folder does not contain a git repository');
-				process.exit();
-			}
+	if (v)
+		version = v.slice(0, v.length - 1);
 
-			//all commits to master will be put into latest
-			//when a new version is created, the files will be put in there
-			if (fs.existsSync(`${__dirname}/projects/${name}/${version}`)) {
-				//update
-				console.log(`Version ${version} Already Exits: Updating latest`)
-				updateLatest(name);
-			} else {
-				//create new submodule that that points to the version
+	//all commits to master will be put into latest
+	//when a new version is created, the files will be put in there
+	if (fs.existsSync(`${__dirname}/projects/${name}/${version}`)) {
+		//update
+		console.log(`Version ${version} Already Exits: Updating latest`)
+		updateLatest(name);
+	} else {
+		//create new submodule that that points to the version
+		//update latest
+		updateLatest(name);
 
+		//get tags
+		let tags = run('git tag', `${__dirname}/projects/${name}/latest`).stdout.toString();
+		tags = tags.split('\n');
+		tags.pop();
+		//for every tag
+		for (let tag of tags) {
+			//if no folder with name
+			if (!fs.existsSync(`${__dirname}/projects/${name}/${tag}`)) {
 				//add submodule with tag as name
+				run(`git submodule add -f -b master https://github.com/trobol/${name} projects/${name}/${tag}`, __dirname);
 				//checkout tag/<tag> -b master
-				updateLatest(name).then(r => {
-					copyFolderRecursiveSync(`${__dirname}/projects/${name}/latest`, `${__dirname}/projects/${name}/${version}`, `${__dirname}/projects/${name}/${version}`);
-				}).catch(e => {
-					console.log(e);
-				}).then(() => {
-					run(['git', 'commit', `--message=Updated ${name}`], __dirname)
-						.then(() => run(`git push`, __dirname).catch(e => console.log(e)))
-						.catch(e => console.log(e))
-				});
+				run(`git checkout tag/${tag}`, `${__dirname}/projects/${name}/${tag}`);
 			}
-		});
+
+		}
+		run(['git', 'commit', `--message=Updated ${name}`], __dirname);
+		run(`git push`, __dirname);
+	}
 }
 function updateLatest(path) {
 	return run(`git submodule update -f projects/${path}/latest`, __dirname);
 }
 //update submodules then return an object with the name of the repo and the branch hash
 function updateAllLatest() {
-	return new Promise((resolve, reject) => {
-		run('git submodule update -f', __dirname)
-			.then(r => {
-				r = r.split('\n');
-				r.pop();
-				let o = {};
-				for (let e of r) {
-					let nameR = /(?<=projects\/)([A-Za-z0-9]+)(?=\/)/,
-						hashR = /(?<=out ')([A-Za-z0-9]+)(?=')/,
-						name = e.match(nameR)[0],
-						hash = e.match(hashR)[0];
-					if (name && hash)
-						o[name] = hash;
-				}
-				resolve(o);
-			}, reject);
-	});
+	let r = run('git submodule update -f', __dirname).stdout;
+	r = r.split('\n');
+	r.pop();
+	let o = {};
+	for (let e of r) {
+		let nameR = /(?<=projects\/)([A-Za-z0-9]+)(?=\/)/,
+			hashR = /(?<=out ')([A-Za-z0-9]+)(?=')/,
+			name = e.match(nameR)[0],
+			hash = e.match(hashR)[0];
+		if (name && hash)
+			o[name] = hash;
+	}
+	resolve(o);
 }
 function run(args, cwd) {
-	return new Promise((resolve, reject) => {
-		if (typeof args == 'string')
-			args = args.split(' ');
-		let command = args.shift();
-		const { spawn } = require('child_process'),
-			child = spawn(command, args, { cwd });
-		let stdout = '',
-			stderr = '';
-
-
-		child.on('exit', function (code, signal) {
-			if (stderr) {
-				reject(stderr);
-			}
-			resolve(stdout);
-
-		});
-		child.stdout.on('data', (data) => {
-			stdout += data;
-		});
-
-		child.stderr.on('data', (data) => {
-			stderr += data;
-		});
-	});
+	if (typeof args == 'string')
+		args = args.split(' ');
+	let command = args.shift();
+	const { spawnSync } = require('child_process'),
+		child = spawnSync(command, args, { cwd });
+	if (child.stderr.length > 0)
+		console.log(child.stderr.toString());
+	return child;
 }
 
 
